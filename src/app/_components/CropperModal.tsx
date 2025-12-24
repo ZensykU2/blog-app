5a737b503b8cad640c4c17fa508e92371aade74e
ZensykU2 <crto.CT@gmail.com>
2025-12-24 14:21:46 +0100

    fix: error object for createImage

--- src/app/_components/CropperModal.tsx
+++ src/app/_components/CropperModal.tsx
@@ -46,7 +46,9 @@ function createImage(url: string): Promise<HTMLImageElement> {
     return new Promise((resolve, reject) => {
         const image = new Image();
         image.addEventListener("load", () => resolve(image));
-        image.addEventListener("error", (error) => reject(error));
+        image.addEventListener("error", () =>
+            reject(new Error("Failed to load image"))
+        );
         image.crossOrigin = "anonymous";
         image.src = url;
     });
